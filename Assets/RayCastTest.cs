e06e555350a2bea108a3b254a830680c8a93b832
Oğuz Kumcular <oguzkumcular@gmail.com>
2018-04-27 15:25:01 +0300

    Creating Folders and assests for help.

---
+++ Assets/RayCastTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCastTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Ray ray = new Ray(transform.position, Vector3.down);
+        RaycastHit HitInfo;
+        if (Physics.Raycast(ray,out HitInfo))
+        {
+            Debug.Log("Someting Else on down like " + HitInfo.transform.name); 
+        }
+		
+	}
+}
