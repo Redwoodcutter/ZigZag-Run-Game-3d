c69d61c8a855d32ade96f5569856a1ba4d671492
Oğuz Kumcular <2012010217035@ogrenci.karabuk.edu.tr>
2018-04-27 15:22:14 +0300

    Add files via upload

---
+++ ZigZag Help/Scripts/CubeThrower.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeThrower : MonoBehaviour {
+
+    public GameObject CubePrefabs;
+
+	// Use this for initialization
+	void Start () {
+        //Invoke("CreateCube", 2);
+        InvokeRepeating("CreateCube", 2, 1);
+	}
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject Go = Instantiate(CubePrefabs);
+            Go.GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, 1) * 1000);
+            Destroy(Go, 3);
+
+
+        }
+       
+    }
+    void CreateCube()
+    {
+        GameObject Go = Instantiate(CubePrefabs);
+        Go.GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, 1) * 1000);
+        Destroy(Go, 3);
+    }
+}
---
+++ ZigZag Help/Scripts/DiceThrower.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DiceThrower : MonoBehaviour {
+
+    public Text hiScore, score;
+    int HighScore, Score;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Score = Random.Range(1, 50);
+            score.text = Score.ToString();
+
+            if (Score > HighScore)
+            {
+                HighScore = Score;
+                hiScore.text = HighScore.ToString();
+            }
+        }
+
+	}
+}
